3c61484a6f00363ac072120eebbd00bb7498e85b
Yari <narducciyarigmail.com>
2022-12-12 16:29:26 +0100

    inizializzazione

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+
+    export default{
+        name:"AppHeader",
+    }
+
+
+
+</script>
+
+<template>
+  <header>
+    <img src="..src/assets/img/" alt="logo">
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
